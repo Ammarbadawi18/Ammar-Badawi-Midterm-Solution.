ec5ab630eb24b8bdad1b50c445fcd68bec47dc96
ammarbadawi18 <ammarbadawi18@gmail.com>
2023-11-12 10:17:32 +0000

    handling user entries

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ def handleNoTabsError():
   if not tabs:
     print("No tabs yet.")
     return
-    
+
+# Function to deal with user entries
+def getTabIndex():
+  index = input("Enter the index of the tab (press Enter to deal with the last opened tab): " )
+  while not index.isdigit() and index != "":
+    print("Please enter a valid integer for the tab index.")
+    index = input("Enter the index of the tab (press Enter to deal with the last opened tab): ")
+  return index
+
 # Function to display the menu
 def displayMenu():
   print("Welcome to the Advanced Browser Tabs Simulation!")
